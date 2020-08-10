4a320784c73014c09b90f090fcbc8f6045ad9452
Elvis Vizcaino <elvizcaino@gmail.com>
2020-08-10 09:39:27 -0500

    docs agregado

---
+++ docs/precache-manifest.2da755c8dde5c1ae1c469dd5191f0cca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7077cf2a8d80f50e51bf1c83d3b7c5c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff55dbd20fdcd1215774",
+    "url": "/static/css/main.7aa7d638.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff55dbd20fdcd1215774",
+    "url": "/static/js/main.819242cc.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
